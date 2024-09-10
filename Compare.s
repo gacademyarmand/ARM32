008f69e82c9d89b76ce85838c228492e1e951cc8
gacademyarmand <gacademyarmand@gmail.com>
2024-09-10 09:22:24 +0100

    Update Compare.s

--- Compare.s
+++ Compare.s
@@ -9,15 +9,15 @@ _start:
 	//if r0 > r1 -> result = -	
 	//if r0 == r1 -> result = 0
 	//the result set the cpsr flag
-	cmp r0, r1 //200001d3 -> 00100000000000000000000111010011 according to ref 29bit=1
+	cmp r0, r1 //200001d3 -> 00100000000000000000000111010011 according to ref CPSR 29bit=1
 	
 	mov r0, #4
 	mov r1, #5
-	cmp r0, r1 //800001d3 -> 10000000000000000000000111010011 according to ref 31bit=1
+	cmp r0, r1 //800001d3 -> 10000000000000000000000111010011 according to ref CPSR 31bit=1
 	
 	mov r0, #5
 	mov r1, #5
-	cmp r0, r1 //600001d3 -> 01100000000000000000000111010011 according to ref 30/29bit=1
+	cmp r0, r1 //600001d3 -> 01100000000000000000000111010011 according to ref CPSR 0/29bit=1
 	
 	
 	
